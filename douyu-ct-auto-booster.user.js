31fcc309b71782cb903d08c36b322be2103c3032
Tunghsiao Liu <t@sparanoid.com>
2017-11-02 19:43:42 +0800

    feat(js): check relative page

--- douyu-ct-auto-booster.user.js
+++ douyu-ct-auto-booster.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Douyu CT Auto Booster
 // @namespace    https://sparanoid.com/work/douyu-ct-auto-booster/
-// @version      1.0.0
+// @version      1.0.1
 // @description  Auto start China Telecom upload boosting service
 // @author       Tunghsiao Liu
 // @include      *://*.douyu.com/*/*
@@ -17,10 +17,6 @@ window.addEventListener('load', function(e) {
 
   console.log('Douyu CT Auto Booster loaded!');
 
-  var el = document.querySelector('#up_div > a');
-  var text = el.textContent;
-  console.log('Current button status: ' + text);
-
   // https://stackoverflow.com/a/2706236/412385
   function eventFire(el, etype){
     if (el.fireEvent) {
@@ -32,29 +28,39 @@ window.addEventListener('load', function(e) {
     }
   }
 
-  if (text === '启用服务') {
-    console.log('Douyu CT Auto Booster not enabled, activating...');
-    eventFire(el, 'click');
-  } else {
-    console.log('Douyu CT Auto Booster enabled, refreshing page in ' + reloadInterval + ' mins...');
+  var el = document.querySelector('#up_div > a');
 
-    // https://regex101.com/r/ZjdDTQ/1
-    var timeText = text.match(/\d+:\d+:\d+/g) || ['00:00:00'];
-    var timePart = timeText[0].split(':');
-    var remainingTime = (+timePart[0]) * 60 * 60 + (+timePart[1]) * 60 + (+timePart[2]);
-    console.log('Remainning time in ' + remainingTime + ' secs');
+  if (el) {
 
-    // If remainingTime is long enough, lazy refresh the page to get the most
-    // accurate remaining time.
-    if (remainingTime > 600) {
-      setTimeout(function() {
-        location.reload();
-      }, 1000 * 60 * reloadInterval);
+    var text = el.textContent;
+    console.log('Current button status: ' + text);
+
+    if (text === '启用服务') {
+      console.log('Douyu CT Auto Booster not enabled, activating...');
+      eventFire(el, 'click');
     } else {
-      setTimeout(function() {
-        location.reload();
-      }, 1000 * 10);
+      console.log('Douyu CT Auto Booster enabled, refreshing page in ' + reloadInterval + ' mins...');
+
+      // https://regex101.com/r/ZjdDTQ/1
+      var timeText = text.match(/\d+:\d+:\d+/g) || ['00:00:00'];
+      var timePart = timeText[0].split(':');
+      var remainingTime = (+timePart[0]) * 60 * 60 + (+timePart[1]) * 60 + (+timePart[2]);
+      console.log('Remainning time in ' + remainingTime + ' secs');
+
+      // If remainingTime is long enough, lazy refresh the page to get the most
+      // accurate remaining time.
+      if (remainingTime > 600) {
+        setTimeout(function() {
+          location.reload();
+        }, 1000 * 60 * reloadInterval);
+      } else {
+        setTimeout(function() {
+          location.reload();
+        }, 1000 * 10);
+      }
     }
+  } else {
+    console.log('Not Douyu CT Booster page, stop processing...');
   }
 
 }, false);
